a5f04f3dcef28c72786c30018953f58bbb71d87d
Qamar Al Amassi <qamar.alamassi@tentimejo.com>
2025-01-16 20:19:07 +0200

    Initial Commit

---
+++ PlanetsApp/PlanetsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetsAppApp.swift
+//  PlanetsApp
+//
+//  Created by Qamar Al Amassi on 16/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlanetsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlanetsAppTests/PlanetsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetsAppTests.swift
+//  PlanetsAppTests
+//
+//  Created by Qamar Al Amassi on 16/01/2025.
+//
+
+import Testing
+@testable import PlanetsApp
+
+struct PlanetsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
